d17bec60f86b0753eebcc9a6cc1d37f1b9190158
Jang-Vijay Singh <jvsing@gmail.com>
2020-06-07 01:24:10 +0100

    Covid-19 api

--- urls.py
+++ urls.py
@@ -2,9 +2,11 @@
 from django.conf.urls import url, include
 from aldryn_django.utils import i18n_patterns
 import aldryn_addons.urls
-
+from django.urls import path
+import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     # add your own patterns here
 ] + aldryn_addons.urls.patterns() + i18n_patterns(
     # add your own i18n patterns here
